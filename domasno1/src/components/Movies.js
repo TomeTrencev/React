78583a05827794b9691e45a1efb348f501e176ac
unknown <trencevtome@yahoo.com>
2022-04-12 22:08:53 +0200

    update

--- domasno1/src/components/Movies.js
+++ domasno1/src/components/Movies.js
@@ -28,11 +28,3 @@ Movies.propTypes = {
     movies: PropTypes.arrayOf(PropTypes.object).isRequired
 }
 
-{/* < ol> {props.movies.map((film,i) => (<h1 key={i}>
-    <h1>{film.name} </h1>
-    <h3 style={{backgroundColor:'red'}}>Released in  :{film.date}</h3>
-    <p style={{ color: 'blue' }}>Plot : {film.plot}</p>
-    <br></br>
-    IMBD:<a href='{film.imdbLink}'>{film.imdbLink}</a> <br></br> <img src={film.imgUrl} width='200px'></img> </h1>))}
-
-</ol> */}
