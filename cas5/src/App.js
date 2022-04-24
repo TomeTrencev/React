19f1a788abb2cf679dfe7d0f21b2b41582c8656a
unknown <trencevtome@yahoo.com>
2022-04-24 10:14:58 +0200

    domasno

---
+++ cas5/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState}  from "react";
+import { Login } from "./components/Login";
+
+
+export function App() {
+
+
+  return(
+    <div id="domasno">
+      <Login />
+      
+    </div>
+  )
+}
--- domasno2/src/App.js
+++ domasno2/src/App.js
@@ -1,101 +1,101 @@
-import React,{useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export function App() {
 
   const [ime, setIme] = useState('');
   const [prezime, setPrezime] = useState('');
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState ('');
+  const [password, setPassword] = useState('');
   const [godini, setGodini] = useState('');
   const [showTable, setShowTable] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     setShowTable(true)
   }, [])
- 
 
-  return(
+
+  return (
     <div id="app">
       <h2>Table</h2>
-     <input
-     type='text'
-     placeholder = 'Enter Ime'
-     value={ime}
-     onChange={(e)=>{setIme(e.target.value)}}
-     
-     />
-     <input
-     type='text'
-     placeholder="Enter Prezime"
-     value={prezime}
-     onChange={(e)=>{setPrezime(e.target.value)}}
-     />
-     <input
-     type='text'
-     placeholder="Enter Email"
-     value={email}
-     onChange={(e)=>{setEmail(e.target.value)}}
-     />
-     <input
-     type='text'
-     placeholder="Enter Password"
-     value={password}
-     onChange={(e)=>{setPassword(e.target.value)}}
-     />
+      <input
+        type='text'
+        placeholder='Enter Ime'
+        value={ime}
+        onChange={(e) => { setIme(e.target.value) }}
 
-     <input
-     type='text'
-     placeholder="Enter godini"
-     value={godini}
-     onChange={(e)=>{setGodini(e.target.value)}}
-     />
-    
-     <br/>
-     {showTable && 
-     <table border=''>
-       <thead>
-       <tr>
-         <th>
-           Ime
-         </th>
-         <th>
-           Prezime
-         </th>
-         <th>
-           Email
-         </th>
-         <th>
-           Password
-         </th>
-         <th>
-           Godini
-         </th>
-       </tr>
-       </thead>
-       <tbody>
-         <tr>
-           <td>
-             {ime}
-           </td>
-           <td>
-             {prezime}
-           </td>
-           <td>
-             {email}
-           </td>
-           <td>
-             {password}
-           </td>
-           <td>
-             {godini}
-           </td>
-         </tr>
-       </tbody>
-     </table>
-}
-     <button onClick={() => setShowTable(!showTable)}>Show Table</button>
-      
+      />
+      <input
+        type='text'
+        placeholder="Enter Prezime"
+        value={prezime}
+        onChange={(e) => { setPrezime(e.target.value) }}
+      />
+      <input
+        type='text'
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => { setEmail(e.target.value) }}
+      />
+      <input
+        type='text'
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => { setPassword(e.target.value) }}
+      />
+
+      <input
+        type='text'
+        placeholder="Enter godini"
+        value={godini}
+        onChange={(e) => { setGodini(e.target.value) }}
+      />
+
+      <br />
+      {showTable &&
+        <table border=''>
+          <thead>
+            <tr>
+              <th>
+                Ime
+              </th>
+              <th>
+                Prezime
+              </th>
+              <th>
+                Email
+              </th>
+              <th>
+                Password
+              </th>
+              <th>
+                Godini
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                {ime}
+              </td>
+              <td>
+                {prezime}
+              </td>
+              <td>
+                {email}
+              </td>
+              <td>
+                {password}
+              </td>
+              <td>
+                {godini}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      }
+      <button onClick={() => setShowTable(!showTable)}>Show Table</button>
+
     </div>
-    
+
   )
 }
